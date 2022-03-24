ef81bd88ff0addfd18a3614a8988d3bad5329f83
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-24 06:56:17 -0700

    start test react

---
+++ src/App2.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+function App() {
+  return (
+    React.createElement('div',{className:'test'},'te')
+  );
+}
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,2 @@
+let firstName = "mehrdad";
+let lastName = "tabesh";
