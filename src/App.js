a1e2af7d9ae2802ad671e38009aa4ac30194fb83
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-28 14:03:19 -0700

    learn props and implemtaion functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
-import User,{ firstName ,fetchMovies} from './Components/User';
+import User from './Components/User'
+import React, {Component} from 'react';
 
-console.log(User,fetchMovies)
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <h1>mehrdad</h1>
+        <User name="mehrdad" age="26" />
+        <User name="mehrdad" age="26" >
+          this is a children props
+          </User>
+      </div>
+    )
+  }
+}
 
+export default App;
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,17 +1,28 @@
-export let firstName = "mehrdad";
-let lastName = "tabesh";
+import React from 'react';
 
-
-export function fetchMovies(){
-    
+//functinal compnent
+let User = function(props){
+    return(
+        <div>
+            hi {props.name} your age is {props.age}
+            <br/>
+            {props.children}
+        </div>
+    )
 }
 
-export default { firstName: firstName, lastName: lastName }
+/* return 
+         string 
+         property 
+         value changeable to string 
+         convert to json 
+         */
 /**
  * 
  * تعریف ماژول:
  * ماژول ها فایل هایی هستن که محتویات درون خودشون را بیرون می فرستند
  */
 
+export default User;
 
 
